0cab48f0a8bf0053404794f47f66d97e9f750a44
Doaitse Swierstra <804883+doaitse@users.noreply.github.com>
2010-08-09 13:37:47 +0000

    Branch voor Doaitse

--- Text.ParserCombinators.UU/Text/ParserCombinators/UU/Parsing.hs
+++
@@ -1,6 +0,0 @@
-module Text.ParserCombinators.UU.Parsing ( module Text.ParserCombinators.UU.Core
-                                         , module Text.ParserCombinators.UU.BasicInstances
-                                         , module Text.ParserCombinators.UU.Derived) where
-import Text.ParserCombinators.UU.Core
-import Text.ParserCombinators.UU.BasicInstances
-import Text.ParserCombinators.UU.Derived
