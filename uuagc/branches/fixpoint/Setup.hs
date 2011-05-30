4d7374092764b81d8d1b3640ab33b608ec5cd8ed
g_jcardona <g_jcardona@30b93d28-2606-0410-b7f1-95e0edb40354>
2011-05-30 02:32:21 +0000

    A simple automatic test framework added.

--- uuagc/branches/fixpoint/Setup.hs
+++ uuagc/branches/fixpoint/Setup.hs
@@ -1,7 +1,15 @@
 -- #!/bin/env runhaskell
 
-import Distribution.Simple(defaultMainWithHooks, UserHooks)
+import Distribution.Simple(defaultMainWithHooks, UserHooks(..))
 import Distribution.Simple.UUAGC
+import UUAGCTests(uuagcPreTest
+                 ,uuagcTestHook
+                 ,uuagcPostTest
+                 )
 
 main :: IO ()
-main = defaultMainWithHooks uuagcUserHook 
+main = defaultMainWithHooks $ uuagcUserHook { preTest  = uuagcPreTest
+                                            , testHook = uuagcTestHook
+                                            , postTest = uuagcPostTest
+                                            }
+
